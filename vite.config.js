cfaaeac0c7bcc038cf7cf5e73a4cfe1afbe84977
DSeptem7 <proyectosdedan@gmail.com>
2026-01-20 12:41:49 -0600

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Esto separa las librerías de 'node_modules' en un archivo aparte
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
