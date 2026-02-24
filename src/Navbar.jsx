bb392313449f7633611d8c411b4e1d6de995b17c
DSeptem7 <proyectosdedan@gmail.com>
2026-02-24 11:58:34 -0600

    restauración navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -85,19 +85,7 @@ const Navbar = ({ setSearchTerm, isDarkMode, setIsDarkMode, productCount }) => {
         <div className="nav-controls">
           
           <div ref={searchRef} className={`search-box ${isSearchExpanded ? 'expanded' : ''}`}>
-            {/* NUEVO: Botón de regreso (solo visible cuando está expandido) */}
-            {isSearchExpanded && (
-              <button 
-                className="back-search-btn" 
-                onClick={() => setIsSearchExpanded(false)}
-                type="button"
-              >
-                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
-                  <line x1="19" y1="12" x2="5" y2="12"></line>
-                  <polyline points="12 19 5 12 12 5"></polyline>
-                </svg>
-              </button>
-            )}
+            
             
             <input 
               ref={inputRef} 
