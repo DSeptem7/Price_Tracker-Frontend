0ce1b5d8b07c8fb17ffbbe48221522c156e965b1
DSeptem7 <proyectosdedan@gmail.com>
2026-04-14 13:00:14 -0600

    se agrega api_base

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,6 +4,7 @@ import { useAuth } from './context/AuthContext';
 import './Navbar.css';
 
 // NOTA: Ya no necesitamos recibir 'products' aquí porque la búsqueda es en el servidor
+const API_BASE = import.meta.env.VITE_API_URL;
 const Navbar = ({ setSearchTerm, isDarkMode, setIsDarkMode, productCount }) => {
   const { user, loginWithGoogle, logout, isAdmin } = useAuth();
   const navigate = useNavigate();
