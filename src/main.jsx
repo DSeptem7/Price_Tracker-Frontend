5b722def08a4c9c61ace3e6e8dda44c4d6d46a2f
DSeptem7 <proyectosdedan@gmail.com>
2026-05-12 16:25:19 -0600

    animation en main..jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App.jsx'
+import "./styles/animations.css";
 
 const savedTheme = localStorage.getItem("isDarkMode");
 
