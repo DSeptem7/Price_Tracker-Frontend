ba4cceb3741ea59a8a8e01dcc39322f15ec035d6
DSeptem7 <proyectosdedan@gmail.com>
2026-05-18 12:27:11 -0600

    cambio de productdetail

--- src/pages/product-detail/ProductDetail.jsx
+++ src/pages/product-detail/ProductDetail.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { formatCurrency } from './utils/format';
+import { formatCurrency } from '../../utils/format';
 import './ProductDetail.css';
-import PriceChartModal from "./components/modal/PriceChartModal";
+import PriceChartModal from "../../components/modal/PriceChartModal";
 import {
   XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, AreaChart, Area,
   ReferenceLine, Label
